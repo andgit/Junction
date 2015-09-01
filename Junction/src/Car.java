f4383da8a3b2139c4d77383c6283e123f4745326
Andrzej <agruntek@wp.pl>
2015-09-01 23:06:01 +0200

    Add handle cars` collision.

--- Junction/src/Car.java
+++ Junction/src/Car.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class Car {
@@ -7,20 +9,34 @@ public class Car {
 	private int x, y;
 	private int width, height;
 	private int boundingHeight;
+	private int boundingDimensions[];
+	private boolean isCollision;
 	private int speed;
+	private final int maxSpeed; 
+	private Color color;
 	private TrafficLights assignedTrafficLights;
 	private boolean stop;		// car stop
+	private long speedTimer;
 	
-    public Car(int x, int y, int width, int height, int speed, TrafficLights assignedTrafficLights) {
+    public Car(int x, int y, int width, int height, int speed, int maxSpeed, Color color, TrafficLights assignedTrafficLights) {
         
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.boundingHeight = 2 * this.height;
+        this.boundingDimensions = new int[4];
+        this.boundingDimensions[0] = x;
+        this.boundingDimensions[1] = y-(this.boundingHeight/2);
+        this.boundingDimensions[2] = width;
+        this.boundingDimensions[3] = height+this.boundingHeight;
+        this.isCollision = false;
         this.speed = speed;
+        this.maxSpeed = maxSpeed;
+        this.color = color;
         this.assignedTrafficLights = assignedTrafficLights;
         this.stop = false;
+        this.speedTimer = System.currentTimeMillis();
     }
 
     public void actionPerformed() {
@@ -48,23 +64,85 @@ public class Car {
 	    		
 	    		this.stop = false;
 	    	}
+	    	
+	    	updateSpeed();//prawdopodobnie to moze zostac-nie psuje:)
     	}
     }
     
     public void paintComponent(Graphics g) {
     	
-		g.setColor(Color.red);
+		g.setColor(color);
 		g.fillRect(x, y, width, height);
+		updateBoundingDimensions();
 		
 		if(GlobalVariables.ENABLE_DEBUG)
 		{
-			g.setColor(Color.blue);
-			g.drawRect(x, y-this.height, width, height+this.boundingHeight);
+			if(this.isCollision) {
+				
+				g.setColor(Color.RED);
+			} else {
+				
+				g.setColor(Color.BLUE);
+			}
+			//g.drawRect(x, y-(this.boundingHeight/2), width, height+this.boundingHeight);
+			g.drawRect(this.boundingDimensions[0], this.boundingDimensions[1], this.boundingDimensions[2], this.boundingDimensions[3]);
 			
 			g.fillOval(500, 310, 20, 20);
 			g.setColor(Color.blue);
 			g.fillOval(500, 330, 20, 20);
 		}
+    }
+    
+	protected Rectangle getBounds() {
+		
+		return new Rectangle(this.boundingDimensions[0], this.boundingDimensions[1], this.boundingDimensions[2], this.boundingDimensions[3]);
+	}
+    
+	public boolean collision(Car car) {
+		
+		return this.isCollision = getBounds().intersects(car.getBounds());
+	}
+	
+	public void accelarate() {
+		
+		if(this.speed < this.maxSpeed) {
+			
+			this.speed += 1;
+		}
+	}
+	
+	public void slowDown() {
+		
+		if(this.speed > 1) {
+			
+			this.speed -= 1;
+		}
+	}
+	
+	private void updateSpeed() {
+		
+		if(System.currentTimeMillis() - this.speedTimer > 4000 && this.isCollision == false) {
+			
+			this.speed = ThreadLocalRandom.current().nextInt(1, this.maxSpeed);
+		}
+	}
+	
+    public void speed(int speed) {
+    	
+    	this.speed = speed;
+    }
+    
+    public int speed() {
+    	
+    	return this.speed;
+    }
+	
+    private void updateBoundingDimensions() {
+    	
+        this.boundingDimensions[0] = x;
+        this.boundingDimensions[1] = y-(this.boundingHeight/2);
+        this.boundingDimensions[2] = width;
+        this.boundingDimensions[3] = height+this.boundingHeight;
     }
     
     /*public boolean isHit(float x, float y) {
@@ -99,5 +177,25 @@ public class Car {
     public int height() {
     	
     	return this.height;
+    }
+    
+    public void stop(boolean stop) {
+    	
+    	this.stop = stop;
+    }
+    
+    public boolean stop() {
+    	
+    	return this.stop;
+    }
+    
+    public Color color() {
+    	
+    	return this.color;
+    }
+    
+    public int maxSpeed() {
+    	
+    	return this.maxSpeed;
     }
 }
--- Junction/src/Main.java
+++ Junction/src/Main.java
@@ -4,6 +4,9 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.ThreadLocalRandom;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
@@ -13,14 +16,28 @@ import javax.swing.Timer;
 
 public class Main extends JComponent {
 
-	private Car firstCar;
+	ArrayList<Car> cars;
 	private TrafficLights firstTrafficLights;
 	private static final long serialVersionUID = 1L;
+	int tempCounter = 0;
+	private long generateNewCarTimer = 0;
+	private final int generatingCarDelta = 2000;
 
 	private void init() {
 		
 		firstTrafficLights = new TrafficLights(TrafficLights.TrafficLightsColors.GREEN, true, false, 600, 595);
-		firstCar = new Car(500, 600, 20, 30, 2, firstTrafficLights);
+		cars = new ArrayList<Car>();
+		cars.add(new Car(500, 100, 20, 30, 2, 6, Color.RED, firstTrafficLights));
+		cars.add(new Car(500, 200, 20, 30, 2, 6, Color.YELLOW, firstTrafficLights));
+		cars.add(new Car(500, 300, 20, 30, 3, 6, Color.GREEN, firstTrafficLights));
+		cars.add(new Car(500, 400, 20, 30, 3, 6, Color.BLACK, firstTrafficLights));
+		cars.add(new Car(500, 500, 20, 30, 3, 6, Color.PINK, firstTrafficLights));
+		cars.add(new Car(500, 600, 20, 30, 3, 6, Color.ORANGE, firstTrafficLights));
+		//cars.add(new Car(500, 400, 20, 30, 4, 6, Color.GREEN, firstTrafficLights));
+		//cars.add(new Car(500, 500, 20, 30, 5, 6, Color.BLACK, firstTrafficLights));
+		//cars.add(new Car(470, 600, 20, 30, 6, 6, Color.RED, firstTrafficLights));
+		//cars.add(new Car(470, 500, 20, 30, 4, 6, Color.PINK, firstTrafficLights));
+		generateNewCarTimer = System.currentTimeMillis();
 	}
 	
 	public Main() {
@@ -32,7 +49,11 @@ public class Main extends JComponent {
 			@Override
 			public void actionPerformed(ActionEvent actEvt) {
 				
-				firstCar.actionPerformed();
+				for(Car c: cars) {
+					
+					c.actionPerformed();
+				}
+				handleCollision();
 				firstTrafficLights.actionPerformed();
 				repaint();
 			}
@@ -49,18 +70,43 @@ public class Main extends JComponent {
 		
 		super.paintComponent(g);
 		g.drawImage(GlobalVariables.BACKGROUND_IMAGE, 0, 0, null);
-		firstCar.paintComponent(g);
+		for(Car c: cars) {
+			
+			c.paintComponent(g);
+		}
 		firstTrafficLights.paintComponent(g);
 		
 		if(GlobalVariables.ENABLE_DEBUG)
 		{
 	        String message = "H=" + Integer.toString(GlobalVariables.SCREEN_WIDTH) + " W=" + Integer.toString(GlobalVariables.SCREEN_HEIGHT);
 	        Debug.doDrawing((Graphics2D) g, message, Color.RED, 20.f, 30.f);
-	        message = "x=" + Integer.toString(firstCar.x()) + " y=" + Integer.toString(firstCar.y());
-	        Debug.doDrawing((Graphics2D) g, message, Color.RED, 20.f, 50.f);
-	        message = "PAUSED=" + GlobalVariables.IS_PAUSED;
-	        Debug.doDrawing((Graphics2D) g, message, Color.RED, 20.f, 70.f);
+	        
+	        float y = 50.f;
+	        for(Car c: cars) {
+	        	
+		        message = "x=" + Integer.toString(c.x()) + " y=" + Integer.toString(c.y());
+		        Debug.doDrawing((Graphics2D) g, message, Color.RED, 20.f, y);
+		        y += 20;
+	        }
+
+	        Debug.doDrawing((Graphics2D) g, "" + tempCounter, Color.RED, 20.f, 90.f);
+	        //message = "PAUSED=" + GlobalVariables.IS_PAUSED;
+	        //Debug.doDrawing((Graphics2D) g, message, Color.RED, 20.f, 70.f);
 		}
+		//todo
+		for(int i=0;i<cars.size();++i) {
+			
+			if(cars.get(i).y() <= 10) {
+				
+				int s = cars.get(i).speed();
+				int ms = cars.get(i).maxSpeed();
+				Color c = cars.get(i).color();
+				cars.remove(i);
+				//cars.add(new Car(500, 100, 20, 600, s, ms, c, firstTrafficLights));
+			}
+		}
+	
+		handleCarList();
 	}
 
 	private static void createAndShowGui() {
@@ -75,6 +121,69 @@ public class Main extends JComponent {
 		frame.setVisible(true);
 	}
 
+	protected void handleCollision() {
+		
+		int carsSize = cars.size();
+		if(carsSize > 1) {
+			
+			for(int i=0;i<carsSize;++i) {
+				
+				for(int j=i+1;j<carsSize;++j) {
+					
+					if(cars.get(i).x() == cars.get(j).x()) {//trzeba sie jeszcze zastano
+						
+						if(cars.get(i).y() <= cars.get(j).y()) {
+
+							if(cars.get(i).collision(cars.get(j))) {
+								
+								if(Math.abs((cars.get(i).y() - cars.get(j).y())) <= cars.get(i).height()) {
+									
+									//int shift = Math.abs((cars.get(i).y() - cars.get(j).y()));
+									cars.get(j).y(cars.get(j).y() + /*cars.get(i).height()*/ 20);
+									++tempCounter;
+									//cars.get(i).slowDown();
+								}
+								
+								if(cars.get(i).stop()) {
+									
+									cars.get(j).stop(true);
+								}
+								if(cars.get(j).speed() > cars.get(i).speed()) {
+									
+									cars.get(j).speed(cars.get(i).speed());
+								}
+							} else {
+								
+								//cars.get(i).accelarate();		//it cause bug
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	protected void handleCarList() {
+		
+		if(this.generateNewCarTimer > generatingCarDelta) {
+			
+			this.generateNewCarTimer = System.currentTimeMillis();
+			boolean genaretaNewCar = false;
+			for(int i=0;i<cars.size();++i) {
+				
+				if(cars.get(i).x() == 500 && cars.get(i).y() < 700 && cars.get(i).y() > 500) {
+					
+					genaretaNewCar = true;
+				}
+			}
+			if(genaretaNewCar == false) {
+				
+				int rand = ThreadLocalRandom.current().nextInt(3, 6);
+				cars.add(new Car(500, 600, 20, 30, rand, 6, Color.ORANGE, firstTrafficLights));
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		
 		SwingUtilities.invokeLater(new Runnable() {
